1bbfb802ad1009a0f20da035762a5d0e548de58d
Elliott Thompson <ellth92@gmail.com>
2015-07-13 16:40:39 +0100

    animation is complete

--- banner.js
+++ banner.js
@@ -2,41 +2,44 @@ var bannerApp = angular.module('bannerApp', []);
 
 bannerApp.controller('bannerController', ['$scope','$window','$timeout', function($scope,$window,$timeout){
   $scope.bannerdata = [
-  {'index':0, 'char':'l'},
-  {'index':1, 'char':'e'},
-  {'index':2, 'char':'t'},
-  {'index':3, 'char':'t'},
-  {'index':4, 'char':'l'},
-  {'index':5, 'char':'.'},
-  {'index':6, 'char':'i'},
-  {'index':7, 'char':'o'}
+  {'index':1, 'charclasses':'char', 'char':'e'},
+  {'index':0, 'charclasses':'char', 'char':'l'},
+  {'index':4, 'charclasses':'char', 'char':'l'},
+  {'index':6, 'charclasses':'char', 'char':'i'},
+  {'index':7, 'charclasses':'char', 'char':'o'},
+  {'index':2, 'charclasses':'char', 'char':'t'},
+  {'index':3, 'charclasses':'char', 'char':'t'},
+  {'index':5, 'charclasses':'char', 'char':'.'}
   ];
+  $scope.charclasses = "char";
   var surname = [
-    {'index':8, 'char':'t'},
-    {'index':9, 'char':'h'},
-    {'index':10, 'char':'o'},
-    {'index':11, 'char':'m'},
-    {'index':12, 'char':'p'},
-    {'index':13, 'char':'s'},
-    {'index':14, 'char':'o'},
-    {'index':15, 'char':'n'}
+    {'index':8, 'charclasses':'animated tada char', 'char':'T'},
+    {'index':9, 'charclasses':'animated tada char', 'char':'h'},
+    {'index':10, 'charclasses':'animated tada char', 'char':'o'},
+    {'index':11, 'charclasses':'animated tada char', 'char':'m'},
+    {'index':12, 'charclasses':'animated tada char', 'char':'p'},
+    {'index':13, 'charclasses':'animated tada char', 'char':'s'},
+    {'index':14, 'charclasses':'animated tada char', 'char':'o'},
+    {'index':15, 'charclasses':'animated tada char', 'char':'n'}
   ];
-
-  var newIndices = [1,0,5,6,2,7,3,4];
 
   $timeout(function() {
     for(i = 0; i < $scope.bannerdata.length; i++) {
-      $scope.bannerdata[i].index = newIndices[i];
-      console.log(newIndices[i]);
+      $scope.bannerdata[i].index = i;
     }
-  }, 2000);
+  }, 4000);
 
   $timeout(function() {
-    $scope.bannerdata = $scope.bannerdata.concat(surname);
-  }, 5000);
+    // $scope.bannerdata = $scope.bannerdata.concat(surname);
+    // for(i = 0; i < $scope.bannerdata.length; i++) {
+    //   $scope.bannerdata[i].charclasses += ' huechange';
+    // }
+    $scope.bannerdata[0].char = 'E';
+    $scope.bannerdata[0].charclasses += ' animated tada';
+  }, 9000);
 
   $scope.getLeftPosition = function(index){
-    return (index * 20 + 200) + 'px';
+    return (index * 25) + 'px';
   }
 
 }]);
