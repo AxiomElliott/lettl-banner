d17992a19995df16a298d08b5c3e129d70c0328b
Elliott Thompson <ellth92@gmail.com>
2015-07-13 21:01:00 +0100

    changes the sizing for the banner

--- banner.js
+++ banner.js
@@ -3,43 +3,54 @@ var bannerApp = angular.module('bannerApp', []);
 bannerApp.controller('bannerController', ['$scope','$window','$timeout', function($scope,$window,$timeout){
   $scope.bannerdata = [
   {'index':1, 'charclasses':'char', 'char':'e'},
-  {'index':0, 'charclasses':'char', 'char':'l'},
-  {'index':4, 'charclasses':'char', 'char':'l'},
-  {'index':6, 'charclasses':'char', 'char':'i'},
-  {'index':7, 'charclasses':'char', 'char':'o'},
-  {'index':2, 'charclasses':'char', 'char':'t'},
-  {'index':3, 'charclasses':'char', 'char':'t'},
-  {'index':5, 'charclasses':'char', 'char':'.'}
+  {'index':-1, 'charclasses':'char', 'char':'l'},
+  {'index':2, 'charclasses':'char', 'char':'l'},
+  {'index':3, 'charclasses':'char', 'char':'i'},
+  {'index':3, 'charclasses':'char', 'char':'o'},
+  {'index':-3, 'charclasses':'char', 'char':'t'},
+  {'index':-3, 'charclasses':'char', 'char':'t'},
+  {'index':-2, 'charclasses':'char', 'char':'.'}
   ];
-  $scope.charclasses = "char";
   var surname = [
-    {'index':8, 'charclasses':'animated tada char', 'char':'T'},
-    {'index':9, 'charclasses':'animated tada char', 'char':'h'},
-    {'index':10, 'charclasses':'animated tada char', 'char':'o'},
-    {'index':11, 'charclasses':'animated tada char', 'char':'m'},
-    {'index':12, 'charclasses':'animated tada char', 'char':'p'},
-    {'index':13, 'charclasses':'animated tada char', 'char':'s'},
-    {'index':14, 'charclasses':'animated tada char', 'char':'o'},
-    {'index':15, 'charclasses':'animated tada char', 'char':'n'}
+    {'index':0, 'charclasses':'char', 'char':'T'},
+    {'index':0, 'charclasses':'char', 'char':'h'},
+    {'index':0, 'charclasses':'char', 'char':'o'},
+    {'index':0, 'charclasses':'char', 'char':'m'},
+    {'index':0, 'charclasses':'char', 'char':'p'},
+    {'index':0, 'charclasses':'char', 'char':'s'},
+    {'index':0, 'charclasses':'char', 'char':'o'},
+    {'index':0, 'charclasses':'char', 'char':'n'}
   ];
+
+  $scope.fasttransition = false;
+  $scope.offset = 60;
 
   $timeout(function() {
     for(i = 0; i < $scope.bannerdata.length; i++) {
-      $scope.bannerdata[i].index = i;
+      $scope.bannerdata[i].index = 0;
     }
   }, 4000);
 
   $timeout(function() {
-    // $scope.bannerdata = $scope.bannerdata.concat(surname);
-    // for(i = 0; i < $scope.bannerdata.length; i++) {
-    //   $scope.bannerdata[i].charclasses += ' huechange';
-    // }
+    $scope.bannerdata = $scope.bannerdata.concat(surname);
+    for(i = 0; i < $scope.bannerdata.length; i++) {
+      $scope.bannerdata[i].charclasses += ' animated tada';
+    }
+    $scope.fasttransition = true;
+    $scope.offset = 0;
     $scope.bannerdata[0].char = 'E';
-    $scope.bannerdata[0].charclasses += ' animated tada';
-  }, 9000);
+  }, 15000);
 
-  $scope.getLeftPosition = function(index){
-    return (index * 25) + 'px';
+  $timeout(function() {
+    $scope.bannerdata = $scope.bannerdata.concat(surname);
+    for(i = 0; i < $scope.bannerdata.length; i++) {
+      $scope.bannerdata[i].charclasses += ' huechange';
+    }
+  }, 17000);
+
+
+  $scope.getCharStyling = function(index){
+    return {'left':(index * 15 + $scope.offset) + 'px', 'transition':$scope.fasttransition?0.5 + 's' : 9 + 's'};
   }
 
 }]);
